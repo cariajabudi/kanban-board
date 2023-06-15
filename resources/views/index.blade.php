668ccf441c9d78b5673873b346d7fbf09aad04db
budi-afk <budiman1347@gmail.com>
2023-06-16 01:18:17 +0700

    configure tailwind

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.app")
+
+@section("content")
+@include("layouts.navbar")
+
+	<h1>Hello Budiman</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	@vite(['resources/css/app.css','resources/js/app.js'])
+	<title>Home</title>
+</head>
+<body>
+	@yield("content")
+</body>
+</html>
